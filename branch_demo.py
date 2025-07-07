a57aed17174b20f220abc3f85090087ef432720e
Iliadm1183 <iliadehmoh@gmail.com>
2025-07-06 19:29:00 -0700

    Add median function

--- branch_demo.py
+++ branch_demo.py
@@ -1,1 +1,12 @@
 print("hello_world")
+import math
+def median(data):
+    data = sorted(data)
+    n = len(data)
+    if n % 2 == 0:
+        middle1 = n // 2 - 1
+        middle2 = n // 2
+        return (data[middle1] + data[middle2]) / 2
+    else:
+        middle = n // 2
+        return data[middle]
